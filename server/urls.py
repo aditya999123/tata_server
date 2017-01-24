ae289cd22875782096d74099f2736b5903188a63
Aditya <aditya999123@gmail.com>
2017-01-24 09:59:45 +0530

    user databse ready

--- server/urls.py
+++ server/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from add_user.views import add_user_fun
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^add_user/', add_user_fun),
 ]
