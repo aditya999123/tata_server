8d1e2532b6ae52dd1e7028a5ea4f45202d6c5555
Aditya <aditya999123@gmail.com>
2017-01-24 10:04:15 +0530

    initial user_add commit for get

--- add_user/views.py
+++ add_user/views.py
@@ -7,9 +7,13 @@ def add_user_fun(request):
 	respone={}
 	if request.method=='GET':
 		try:
-			pass
-
-
+			data=[]
+			for o in user_level.objects.all():
+				tmp_json={}
+				tmp_json['user_level']=o.user_level
+				tmp_json['name']=o.name
+				data.append(tmp_json)
+			respone['data']=data
 		except Exception,e:
 			respone['success']=False
 			respone['message']=str(e)
